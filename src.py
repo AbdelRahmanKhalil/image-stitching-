a5b9c43d70c266db0163419ab35abe023eb36980
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-11-25 19:17:27 +0200

    changed from 2 points to 4 points

--- src.py
+++ src.py
@@ -6,13 +6,13 @@ from skimage import io
 # img1_rgb = cv2.cvtColor(img1_BGR, cv2.COLOR_BGR2RGB)
 
 # Create point matrix get coordinates of mouse click on image
-point_matrix = np.zeros((2,2),np.int)
+point_matrix = np.zeros((4,2),np.int)
  
 counter = 0
 def mousePoints(event,x,y,flags,params):
     global counter
     # Left button mouse click event opencv
-    if event == cv2.EVENT_LBUTTONDOWN and counter < 2:
+    if event == cv2.EVENT_LBUTTONDOWN and counter < 4:
         point_matrix[counter] = x,y
         counter = counter + 1
         print("counter=", counter)
@@ -21,16 +21,16 @@ def get_points(image_path):
     # Read image
     img = cv2.imread(image_path)
     
-    while counter < 3:
-        for x in range (0,2):
+    while counter < 5:
+        for x in range (0,4):
             cv2.circle(img,(point_matrix[x][0],point_matrix[x][1]),3,(0,255,0),cv2.FILLED)
     
-        if counter == 2:
-            starting_x = point_matrix[0][0]
-            starting_y = point_matrix[0][1]
+        if counter == 4:
+            # starting_x = point_matrix[0][0]
+            # starting_y = point_matrix[0][1]
     
-            ending_x = point_matrix[1][0]
-            ending_y = point_matrix[1][1]
+            # ending_x = point_matrix[1][0]
+            # ending_y = point_matrix[1][1]
             cv2.waitKey(10)
             break
             # Draw rectangle for area of interest
@@ -55,3 +55,5 @@ point_matrix_1 =  point_matrix
 counter = 0
 get_points("image2.jpg")
 point_matrix_2 =  point_matrix
+
+
